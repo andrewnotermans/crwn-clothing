84a96d9f6676ce45aaf6a827f1ee13b804a1a531
Andrew Notermans <andrewnotermans@gmail.com>
2025-06-26 00:26:22 +0200

    05_Directory component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import CategoryItem from './components/category-item/category-item.component';
 
-import './categories.styles.scss';
+import Directory from './components/directory/directory.component';
+
 
 
 const App = () => {
@@ -34,11 +34,7 @@ const App = () => {
 ];
 
   return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category}/>
-      ))}
-    </div>
+    <Directory categories={categories} />
   );
 }
 
